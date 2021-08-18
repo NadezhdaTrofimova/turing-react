7eef33959415f20b7464d90b03ee18ce56bea8e6
NadezhdaTrofimova <trofimnad1991@gmail.com>
2021-08-19 00:08:14 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,47 @@
+// import logo from './logo.svg';
+// import './App.css';
+//
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+//
+// export default App;
+
+// ................Lecture....................
+
+// import React from 'react';
+// import ListClass from './lecture/day1'
+// import List from './lecture/day2'
+// import ListFunctional from "./lecture/day2";
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//         <>
+//             // eslint-disable-next-line
+//           {/*<ListClass/>*/}
+//           {/*<List/>*/}
+//           {/*<ListFunctional/>*/}
+//         </>
+//     )
+//   }
+// }
+//
+// export default App
